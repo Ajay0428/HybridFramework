069ab195778b4d6448aec57907f8666f355d04e1
Ajay <namalaajay2000@gmail.com>
2025-08-15 06:47:43 +0530

    updated BaseTest File

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,8 @@ package base;
 import java.io.IOException;
 import java.lang.reflect.Method;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -12,26 +14,28 @@ import drivers.DriverFactory;
 
 public class BaseTest {
 
-
+	protected WebDriver driver;
 	DriverFactory driverFactory = new DriverFactory();
 	ConfigReader config = new ConfigReader();
-	
+
 	@BeforeMethod
 	public void setup(Method method, ITestContext context) throws IOException {
-		
-		 String browser = config.getProperty("browser");
-	        if (browser == null || browser.isEmpty()) {
-	            browser = "chrome"; 
-	        }
 
-	        DriverFactory.initializeDriver(browser);
-	        DriverFactory.getDriver().get(config.getProperty("baseUrl"));
+		String browser = config.getProperty("browser");
+		if (browser == null || browser.isEmpty()) {
+			browser = "chrome";
+		}
+
+		DriverFactory.initializeDriver(browser);
+		driver = DriverFactory.getDriver();
+		PageFactory.initElements(driver, this);
+		DriverFactory.getDriver().get(config.getProperty("baseUrl"));
 	}
 
 	@AfterMethod
 	public void teardown() {
-		
+
 		driverFactory.quitDriver();
-		
+
 	}
 }
